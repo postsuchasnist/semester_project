9d8997c58813a5dab3e3fe8936a2b4f956246665
Mariia <144185305+postsuchasnist@users.noreply.github.com>
2025-06-22 23:38:21 +0200

    Delete generator.hs

--- generator.hs
+++
@@ -1,9 +0,0 @@
-module Generator (generate) where 
-
-import System.Random 
-
-type Point = (Double, Double)
-
-generate :: Int -> IO [Point]
-generate n = sequence [ (,) <$> r <*> r | _ <- [1..n] ]
-    where r = randomRIO(-100, 100 :: Double)
